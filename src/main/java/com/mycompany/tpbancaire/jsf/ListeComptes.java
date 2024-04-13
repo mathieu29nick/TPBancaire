26a8a5e3dca83777b0cec9afd6773f49e3952910
Nick Mathieu <rakotomanananick29@gmail.com>
2024-04-13 22:03:24 +0300

    TP4- Écriture des pages JSF et des backing beans associés

---
+++ src/main/java/com/mycompany/tpbancaire/jsf/ListeComptes.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package com.mycompany.tpbancaire.jsf;
+
+import com.mycompany.tpbancaire.entity.CompteBancaire;
+import com.mycompany.tpbancaire.service.GestionnaireCompte;
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author rakot
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable{
+
+    @Inject
+    private GestionnaireCompte bean;
+    private List<CompteBancaire> compteList; 
+    
+    public ListeComptes() {
+    }
+    
+    public List<CompteBancaire> getAllComptes() {
+        if (compteList == null) {
+            compteList = bean.getAllComptes();
+        }
+        return compteList;
+    }
+    
+}
